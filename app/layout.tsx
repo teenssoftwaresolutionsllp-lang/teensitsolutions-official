52ef389c3d452850414a369ec3cdc317af3162c9
Balajimarpally <balajimarpally@gmail.com>
2026-09-15 10:22:18 +0530

    fixed mobile number

--- app/layout.tsx
+++ app/layout.tsx
@@ -56,8 +56,31 @@ export default function RootLayout({
           src="/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.4.1"
           strategy="beforeInteractive"
         />
+        <Script id="meta-pixel" strategy="beforeInteractive">
+          {`!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '924956710682252');
+fbq('track', 'PageView');`}
+        </Script>
       </head>
-      <body>{children}</body>
+      <body>
+        {children}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=924956710682252&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+      </body>
     </html>
   );
 }
